93d352d6febc679f2da5c81fef7b74efcc54de42
Kareem Hussein <kareemh14@hotmail.com>
2025-06-28 22:08:09 -0400

    Fix HTML structure and improve JavaScript function for displaying results

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-const stages=[
-    [
-        "IA", "IB", "IIA", "IIB", "IIIA", "IVA", "IVB"
-    ]
-    [
-        "IIA", "IIB", "IIB", "IIIA", "IIIA", "IVA", "IVB"
-    ]
-    [
-        "IIB", "IIA", "IIA", "IIIA", "IIIB", "IVA", "IVB"
-    ]
-    [
-        "IIIA", "IIB", "IIB", "IIB", "IIB", "IVA", "IVB"
-    ]
-    [
-        "IIB", "IIB", "IIB", "IIC", "IIC", "IVA", "IVB"
-    ]
-]
+const stages = [
+    ["IA", "IB", "IIA", "IIB", "IIIA", "IVA", "IVB"],
+    ["IIA", "IIB", "IIB", "IIIA", "IIIA", "IVA", "IVB"],
+    ["IIB", "IIA", "IIA", "IIIA", "IIIB", "IVA", "IVB"],
+    ["IIIA", "IIB", "IIB", "IIB", "IIB", "IVA", "IVB"],
+    ["IIB", "IIB", "IIB", "IIC", "IIC", "IVA", "IVB"]
+];
 function submit(){
-    let TM=document.getElementById("T/M")
-    let N=document.getElementById("N")
-    document.writeln("<h2>", stages[N][TM], "</h2>")
+    let TM=document.getElementById("T/M").value
+    let N=document.getElementById("N").value
+    document.getElementById("result").innerHTML=stages[N][TM]
 }
